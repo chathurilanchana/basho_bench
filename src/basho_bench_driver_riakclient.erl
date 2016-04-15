f9225bac3f97f0cde00def9e409c3d39fd75dbcc
Chathuri <lanch.gunawardhana@gmail.com>
2016-04-15 17:41:24 +0200

    improved efficiency; use same channel, include payload data

--- src/basho_bench_driver_riakclient.erl
+++ src/basho_bench_driver_riakclient.erl
@@ -26,6 +26,7 @@
 -include("basho_bench.hrl").
 -include("riak_kv_causal_service.hrl").
 -record(state, { client,
+                 socket,
                  max_ts::integer(),
                  client_id::integer(),
                  timer_interval,
@@ -52,7 +53,7 @@ new(Id) ->
     Server_Port=basho_bench_config:get(server_port,50000),
     Cookie  = basho_bench_config:get(riakclient_cookie, 'riak'),
     MyNode  = basho_bench_config:get(riakclient_mynode, [basho_bench, longnames]),
-
+    {ok, Socket} = gen_tcp:connect(Server_Ip, Server_Port, [binary, {active,true}]),
   %% Try to spin up net_kernel
   case net_kernel:start(MyNode) of
     {ok, _} ->
@@ -72,6 +73,7 @@ new(Id) ->
                           max_ts =  0,
                           batch_count = 0,
                           batched_labels = [],
+                          socket = Socket,
                           server_ip = Server_Ip,
                           server_port = Server_Port}}.
 
@@ -85,15 +87,14 @@ run(get, KeyGen, _ValueGen, State) ->
         {error, Reason} ->
             {error, Reason, State}
     end;
-run(put, KeyGen, ValueGen, State=#state{server_ip = Server_Ip,server_port = Server_Port}) ->
+run(put, KeyGen, ValueGen, State=#state{server_ip = Server_Ip,server_port = Server_Port,socket = Socket}) ->
     Key= KeyGen(),
     Timestamp=get_timestamp(),
     UpdatedMaxTS=max(State#state.max_ts,Timestamp),
     Node_id=State#state.client_id,
-    %Label=#label{bkey = Key,timestamp = UpdatedMaxTS,node_id = Node_id},
-     %Label=integer_to_list(Timestamp) ++ ";" ++ integer_to_list(Key),
-     Label=list_to_binary(string:join([integer_to_list(Timestamp),integer_to_list(Key)],";")),
-     %Label={struct,[{<<"partition">>,Node_id},{<<"stablets">>,UpdatedMaxTS}],
+
+     %Label=list_to_binary(string:join([integer_to_list(Timestamp),integer_to_list(Key)],";")),
+     Label=string:join([integer_to_list(Timestamp),integer_to_list(Key)],":"),
      Batched_Labels=[Label|State#state.batched_labels],
 
      Batch_Count=State#state.batch_count+1,
@@ -101,10 +102,20 @@ run(put, KeyGen, ValueGen, State=#state{server_ip = Server_Ip,server_port = Serv
 
    if
         Batch_Count>=Timer_Interval ->
-          Reversed_List=lists:reverse(State#state.batched_labels),
-          Json=mochijson2:encode({struct,[{<<"partition">>,Node_id},{<<"stablets">>,UpdatedMaxTS},{<<"labels">>, {array, Reversed_List}}]}),
-          send_json_to_server(Json,Server_Ip,Server_Port),
-          State1=State#state{batched_labels =[],batch_count = 0 };
+          %Reversed_List=lists:reverse(State#state.batched_labels),
+
+          Label_Str=generate_label_str(State#state.batched_labels,$;),
+          Payload=generate_string(Node_id,UpdatedMaxTS,Label_Str),
+          Payload_Length=length(Payload),
+          Padded_Payload_Header=get_padded_payload_length(Payload_Length),
+          Str_With_Header=string:join([Padded_Payload_Header,Payload],"|"),
+          io:format("label str is ~p ~n",[Str_With_Header]),
+          %Json=mochijson2:encode({struct,[{<<"partition">>,Node_id},{<<"stablets">>,UpdatedMaxTS},{<<"labels">>, {array, Reversed_List}}]}),
+          %send_json_to_server(Json,Server_Ip,Server_Port),
+
+          Socket1=send_json_to_server(Str_With_Header,Server_Ip,Server_Port,Socket),
+
+          State1=State#state{batched_labels =[],batch_count = 0,socket = Socket1 };
             %do json conversion here
         true ->
           State1=State#state{batched_labels = Batched_Labels,batch_count = Batch_Count}
@@ -116,11 +127,28 @@ run(put, KeyGen, ValueGen, State=#state{server_ip = Server_Ip,server_port = Serv
 %% Internal functions
 %% ====================================================================
 
-send_json_to_server(Json,Server_Ip,Server_Port)->
-  {ok, Socket} = gen_tcp:connect(Server_Ip, Server_Port, [binary, {active,true}]),
-  gen_tcp:send(Socket, Json).
+get_padded_payload_length(Payload_Length)->
+  PayloadHeaderStr=integer_to_list(Payload_Length),
+  string:right(PayloadHeaderStr, 8, $0).
 
 
+generate_label_str(Items, Sep) ->
+  lists:flatten(generate_label_str1(Items, Sep, [])).
+
+generate_label_str1([Head | []], _Sep, Acc) ->
+  [Head | Acc];
+generate_label_str1([Head | Tail], Sep, Acc) ->
+  generate_label_str1(Tail, Sep, [Sep, Head | Acc]).
+
+generate_string(NodeId,UpdatedMaxTS,LabelStr)->
+  integer_to_list(NodeId) ++ "|" ++ integer_to_list(UpdatedMaxTS) ++"|" ++ LabelStr.
+
+send_json_to_server(Json,Server_Ip,Server_Port,Socket)->
+  %{ok, Socket} = gen_tcp:connect(Server_Ip, Server_Port, [binary, {active,true}]),  %reuse same channel
+  case gen_tcp:send(Socket, Json) of
+      ok->Socket;
+      _-> {ok, Socket1} = gen_tcp:connect(Server_Ip, Server_Port, [binary, {active,true}]),gen_tcp:send(Socket1, Json),Socket1
+  end.
 
 get_timestamp()->
     {MegaSecs, Secs, MicroSecs}=os:timestamp(),
