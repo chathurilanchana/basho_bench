1af622087d2d3f97c90dde564e3d91e9594fc2ab
Chathuri <lanch.gunawardhana@gmail.com>
2016-05-16 08:42:10 +0200

    changes from eunomia to eventual

--- src/basho_bench_driver_riakclient.erl
+++ src/basho_bench_driver_riakclient.erl
@@ -22,19 +22,19 @@
 -module(basho_bench_driver_riakclient).
 
 -export([new/1,
-         run/4]).
+    run/4]).
 
 -include("basho_bench.hrl").
 
 -record(state, { client,
-                 target_node,
-                 bucket,
-                 replies,
-                 vclock,
-                 max_ts=0,
-                 put_count=0
+    target_node,
+    bucket,
+    replies,
+    vclock,
+    max_ts=0,
+    put_count=0
 
-    }).
+}).
 
 %% ====================================================================
 %% API
@@ -45,7 +45,7 @@ new(Id) ->
     case code:which(riak_client) of
         non_existing ->
             ?FAIL_MSG("~s requires riak_client module to be available on code path.\n",
-                      [?MODULE]);
+                [?MODULE]);
         _ ->
             ok
     end,
@@ -56,10 +56,6 @@ new(Id) ->
     Replies = basho_bench_config:get(riakclient_replies, 2),
     Bucket  = basho_bench_config:get(riakclient_bucket, <<"test">>),
     Cluster_Members= basho_bench_config:get(riakclient_cluster_members),% ping and connect kernal to all members to avoid errors
-    Num_DCs=basho_bench_config:get(num_dcs),
-    Local_Dc_Id=basho_bench_config:get(local_dc_id),
-
-    Dict1=init_vclock(dict:new(),Num_DCs,Local_Dc_Id),
 
     %% Try to spin up net_kernel
     case net_kernel:start(MyNode) of
@@ -86,11 +82,10 @@ new(Id) ->
     case riak:client_connect(TargetNode) of
         {ok, Client} ->
             {ok, #state { client = Client,
-                          target_node=TargetNode,
-                          vclock = Dict1,
-                          put_count = 0,
-                          bucket = Bucket,
-                          replies = Replies }};
+                target_node=TargetNode,
+                put_count = 0,
+                bucket = Bucket,
+                replies = Replies }};
         {error, Reason2} ->
             ?FAIL_MSG("Failed get a riak:client_connect to ~p: ~p\n", [TargetNode, Reason2])
     end.
@@ -98,27 +93,23 @@ new(Id) ->
 run(get, KeyGen, _ValueGen, State) ->
     Key = KeyGen(),
     case (State#state.client):get(State#state.bucket, Key,State#state.max_ts, State#state.replies) of
-        {{ok, Val},VNode_VClock} ->
+        {ok, Val} ->
             {_D1,TSBIN} = riak_object:get_value(Val),
             TS=binary_to_term(TSBIN),
-            My_Vec=State#state.vclock,
-            My_Vec1=get_max_vector(My_Vec,VNode_VClock),
             MaxTS=max(TS,State#state.max_ts) ,  %to ensure causality
-            {ok, State#state{max_ts = MaxTS,vclock = My_Vec1}};
-        {{error, notfound},_VClock} ->
+            {ok, State#state{max_ts = MaxTS}};
+        {error, notfound} ->
             {ok, State};
         {error, Reason} ->
             {error, Reason, State}
     end;
 run(put, KeyGen, ValueGen, State) ->
     MaxTS=State#state.max_ts,
-    My_VClock=State#state.vclock,
     Robj = riak_object:new(State#state.bucket, KeyGen(), ValueGen()),
-    case riak_client:put(Robj,{MaxTS,My_VClock}, State#state.replies,{riak_client,[State#state.target_node,undefined]}) of
+    case riak_client:put(Robj,MaxTS, State#state.replies,{riak_client,[State#state.target_node,undefined]}) of
         {ok,Timestamp} ->
             UpdatedMaxTS=max(MaxTS,Timestamp),
             Put_Count= State#state.put_count+1,
-            %io:format("updated Max TS is ~p myid is ~p ~n",[UpdatedMaxTS,self()]),
             {ok, State#state{max_ts = UpdatedMaxTS,put_count = Put_Count}};
         {error, Reason} ->
             {error, Reason, State}
@@ -132,7 +123,7 @@ run(update, KeyGen, ValueGen, State) ->
             case (State#state.client):put(Robj2,MaxTS, State#state.replies) of
                 {ok,Timestamp}->
                     UpdatedMaxTS=max(MaxTS,Timestamp),
-                     %io:format("updated max ts is ~p myid is ~p ~n",[UpdatedMaxTS,self()]),
+                    %io:format("updated max ts is ~p myid is ~p ~n",[UpdatedMaxTS,self()]),
                     Put_Count= State#state.put_count+1,
                     %lager:info("put count is ~p id is ~p ~n",[Put_Count,self()]),
                     {ok, State#state{max_ts = UpdatedMaxTS,put_count = Put_Count}};
@@ -162,10 +153,10 @@ run(delete, KeyGen, _ValueGen, State) ->
             {error, Reason, State}
     end;
 run(test, _KeyGen, _ValueGen, State) ->
-     io:format("calling actual driver ~p ~n",[State]),
+    io:format("calling actual driver ~p ~n",[State]),
     Put_Count=State#state.put_count,
-     io:format("TOTAL PUTS BY THIS THREAD IS ~p ~n",[Put_Count]),
-     ok.
+    io:format("TOTAL PUTS BY THIS THREAD IS ~p ~n",[Put_Count]),
+    ok.
 
 %% ====================================================================
 %% Internal functions
@@ -188,20 +179,3 @@ connect_kernal([Node|Rest])->
     net_kernel:connect_node(Node),
     global:sync(),
     connect_kernal(Rest).
-
-init_vclock(Dict,0,_Local_Id)->Dict;
-
-init_vclock(Dict,Num_DCs,Local_Id)->
-    case Num_DCs of
-        Local_Id-> init_vclock(Dict,Num_DCs-1,Local_Id);
-            _   -> Dict1=dict:store(Num_DCs,0,Dict),
-                   init_vclock(Dict1,Num_DCs-1,Local_Id)
-    end.
-
-get_max_vector(My_Vec,VNode_VClock)->
-    lists:foldl(fun(Key, Vector) ->
-    MyClock = dict:fetch(Key, My_Vec),
-    ReceivedClock = dict:fetch(Key, VNode_VClock),
-    Max= max(MyClock,ReceivedClock),
-    dict:store(Key, Max, Vector)
-    end, dict:new(),dict:fetch_keys(My_Vec)).
